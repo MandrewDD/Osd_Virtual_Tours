245c76934f43e368e9b5ece6ad44b7a2c1e5b691
MandrewDD <Moseenkowam@gmail.com>
2026-06-10 02:17:40 +0300

    Update palati 2

--- Tours/Palati_Carya_2/output/skin.js
+++ Tours/Palati_Carya_2/output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.2.4/15996
 // Filename: simplex_v5_autohide.ggsk
-// Generated Сб июн 6 02:22:14 2026
+// Generated Ср июн 10 02:17:17 2026
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
